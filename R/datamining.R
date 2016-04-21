58091293c9e4ffe7499c1b9fd2d64680727ca014
Joshua García Palacios <Joshi666bcn@hotmail.com>
2016-04-21 22:32:38 +0200

    Add code to download CSV from repository

--- R/datamining.R
+++ R/datamining.R
@@ -1,10 +1,12 @@
-# GLOBAL PARAMETERS
 library(ggplot2)
-# FUNCTIONS
 
 readCSV<-function(input){
 
-  #
+  # Download CSV from repository
+  # fileUrl = "https://raw.githubusercontent.com/TheMaphius/DataMiningProject/master/resources/XXXXXX.csv"
+  # if(!file.exists("./resources")){dir.create("./resources")}
+  # download.file(fileUrl, destfile = "./resources/log_tuneado.csv", method = "curl")
+
   path = paste(getwd(),"/resources/log_tuneado.csv",sep="")
   csv<<-read.csv(file=path, header=FALSE, sep=";", col.names = c("c1","TimeStamp","c3","c4","c5","c6","c7","IP_origen","IP_Publica","c10","IP_interna","c12","c13","Protocolo","c15","Desde","Hacia","c18","c19","c20","c21","c22","c23","c24","Puerto_destino","c26","c27","c28","c29","c30","c31","32","Malicioso","Nivel_de_riesgo","35","c36","c37","Pais_origen","Pais_destino","c40","c41","c42","c43","c44","c45","c46","c47","c48","c49","Sender","Subject","Remitente","c53","c54","c55","c56","c57"), colClasses = c("character", "factor", "factor", "character" )) [,c("TimeStamp","IP_origen","IP_Publica","Protocolo","Desde","Hacia","Puerto_destino","Malicioso","Nivel_de_riesgo","Pais_origen","Pais_destino","Sender","Subject","Remitente")]
   numLines<<-length(csv$TimeStamp)
@@ -16,8 +18,8 @@ dataAdjust<-function(){
   hours<-table((format(strptime(csv$TimeStamp, format='%d/%m/%Y %H:%M'),'%H')))
   percents<-(hours/numLines)*100
   print(sum(percents))
-  ## S3 method for class 'table'
   plot(percents)
+
 }
 
 readCSV()
